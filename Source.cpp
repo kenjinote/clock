1a5d1ee47625e214e734b7b4b393b3a914e0a606
kenjinote <2605401+kenjinote@users.noreply.github.com>
2022-03-23 18:31:33 +0900

    色やフォント、アイコンを設定

--- Source.cpp
+++ Source.cpp
@@ -1,30 +1,57 @@
 ﻿#pragma comment(linker,"\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 
 #include <windows.h>
+#include "resource.h"
 
-#define CLIENT_WIDTH 256
+#define CLIENT_WIDTH 300
 #define CLIENT_HEIGHT 128
+#define FONT_SIZE (-75)
 
 TCHAR szClassName[] = TEXT("clock");
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
+	static HGLOBAL hGlobal = NULL;
+	static HANDLE hFontResource = NULL;
 	static HFONT hFont = NULL;
 	static SYSTEMTIME st = {};
-	static WCHAR szTime[256] = {};
+	static WCHAR szTime[32] = {};
 	switch (msg)
 	{
 	case WM_CREATE:
-		hFont = CreateFontW(-50, 0, 0, 0, FW_NORMAL, 0, 0, 0, SHIFTJIS_CHARSET, 0, 0, 0, 0, L"MS Shell Dlg");
+		{
+			HRSRC hRes = FindResource(0, MAKEINTRESOURCE(IDR_FONT1), RT_FONT);
+			if (!hRes)
+			{
+				return -1;
+			}
+			DWORD dwSize = SizeofResource(0, hRes);
+			if (!dwSize)
+			{
+				return -1;
+			}
+			hGlobal = LoadResource(0, hRes);
+			if (!hGlobal)
+			{
+				return -1;
+			}
+			LPVOID pBuffer = LockResource(hGlobal);
+			if (!pBuffer)
+			{
+				return -1;
+			}
+			DWORD dwFonts;
+			hFontResource = AddFontMemResourceEx(pBuffer, dwSize, 0, &dwFonts);
+		}
+		hFont = CreateFontW(FONT_SIZE, 0, 0, 0, FW_NORMAL, 0, 0, 0, ANSI_CHARSET, 0, 0, 0, 0, L"Let's go Digital");
 		SendMessage(hWnd, WM_TIMER, 0, 0);
 		SetTimer(hWnd, 0x1234, 1, NULL);
 		break;
 	case WM_TIMER:
 		{
 			SYSTEMTIME st_ = {};
+			double vt = 0.0, vt_ = 0.0;
 			GetLocalTime(&st_);
-			double vt = 0.0;
-			double vt_ = 0.0;
 			SystemTimeToVariantTime(&st, &vt);
 			SystemTimeToVariantTime(&st_, &vt_);
 			if (vt != vt_) {
@@ -43,7 +70,11 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 			HFONT hOldFont = (HFONT)SelectObject(hdc, hFont);
 			RECT rect = {};
 			GetClientRect(hWnd, &rect);
+			int nOldBkMode = SetBkMode(hdc, TRANSPARENT);
+			COLORREF cOldColor = SetTextColor(hdc, RGB(38, 41, 52));
 			DrawText(hdc, szTime, -1, &rect, DT_SINGLELINE | DT_VCENTER | DT_CENTER);
+			SetTextColor(hdc, cOldColor);
+			SetBkMode(hdc, nOldBkMode);
 			SelectObject(hdc, hOldFont);
 			EndPaint(hWnd, &ps);
 		}
@@ -57,6 +88,12 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 	case WM_DESTROY:
 		KillTimer(hWnd, 0x1234);
 		DeleteObject(hFont);
+		if (hFont)
+			DeleteObject(hFont);
+		if (hFontResource)
+			RemoveFontMemResourceEx(hFontResource);
+		if (hGlobal)
+			FreeResource(hGlobal);
 		PostQuitMessage(0);
 		break;
 	default:
@@ -68,25 +105,24 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPreInst, LPWSTR pCmdLine, int nCmdShow)
 {
 	MSG msg;
+	HBRUSH hBrush = CreateSolidBrush(RGB(130,135,103));
 	WNDCLASS wndclass = {
 		CS_HREDRAW | CS_VREDRAW,
 		WndProc,
 		0,
 		0,
 		hInstance,
-		0,
+		LoadIcon(hInstance,MAKEINTRESOURCE(IDI_ICON1)),
 		LoadCursor(0,IDC_ARROW),
-		(HBRUSH)(COLOR_WINDOW + 1),
+		hBrush,
 		0,
 		szClassName
 	};
 	RegisterClass(&wndclass);
-
 	DWORD dwWindowStyle = WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN;
 	RECT rect = {};
 	SetRect(&rect, 0, 0, CLIENT_WIDTH, CLIENT_HEIGHT);
 	AdjustWindowRect(&rect, dwWindowStyle, 0);
-
 	HWND hWnd = CreateWindowEx(
 		WS_EX_TOPMOST,
 		szClassName,
@@ -108,5 +144,6 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPreInst, LPWSTR pCmdLine, in
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
 	}
+	DeleteObject(hBrush);
 	return (int)msg.wParam;
 }
